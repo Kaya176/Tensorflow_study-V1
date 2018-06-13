e8879ae90def2b9723081578bf3afd447ee90427
jongHyeok park <37390788+Kaya176@users.noreply.github.com>
2018-06-13 17:08:08 +0900

    Add files via upload
    시드값을 주는게 어쩌면 더 좋을것같기도 하다.

---
+++ 4-1.Softmax Regression.py
@@ -0,0 +1,36 @@
+import tensorflow as tf
+
+#Softmax Classification
+x_data = [[1,2,2,1],[2,1,3,2],[3,1,3,4],[4,1,5,5],[1,7,5,5],
+          [1,2,5,6],[1,6,6,6],[1,7,7,7]]
+# y_data = [2,2,2,1,1,0,0] -> one-hot Encoding
+y_data = [[0,0,1],[0,0,1],[0,0,1],[0,1,0],[0,1,0],[0,1,0],[1,0,0],
+          [1,0,0]]
+
+X = tf.placeholder("float",[None,4])
+Y = tf.placeholder("float",[None,3])
+nb_classes = 3
+
+W = tf.Variable(tf.random_normal([4,3]),name = "weight")
+b = tf.Variable(tf.random_normal([3]),name = "bias")
+
+#tf.nn.softmax computes softmax activations
+# softmax = exp(logit)/reduce_sum(exp(logit),dim)
+
+hypothesis = tf.nn.softmax(tf.matmul(X,W)+b)
+
+cost = tf.reduce_mean(-tf.reduce_sum(Y*tf.log(hypothesis),axis =1))
+
+optimizer = tf.train.GradientDescentOptimizer(learning_rate = 1e-5).minimize(cost)
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+
+    for step in range(10001):
+        sess.run(optimizer,feed_dict={X : x_data,Y : y_data})
+
+        if step % 2000 == 0:
+            print(step,sess.run(cost,feed_dict = {X: x_data, Y: y_data}))
+    a = sess.run(hypothesis,feed_dict = {X : [[1,2,2,1]]})
+
+    print(a,sess.run(tf.argmax(a,1)))
